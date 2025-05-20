5e41ae419851a0463e217390cbe502d1d21332da
0x0desha74 <mudesha74@gmail.com>
2025-05-20 23:08:51 +0300

    EmployeeService  - getAll employees

--- src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeesController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeesController.java
@@ -2,11 +2,10 @@ package com.luv2code.springboot.cruddemo.controller;
 
 import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
 import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,16 +13,16 @@ import java.util.List;
 @RequestMapping("/api")
 public class EmployeesController {
 
-    private final EmployeeDAO employeeDAO;
+    private final EmployeeService employeeService;
 
-
-    public EmployeesController(EmployeeDAO employeeDAO) {
-        this.employeeDAO = employeeDAO;
+    public EmployeesController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
+
 
     @GetMapping("/employees")
     public ResponseEntity<List<Employee>> getEmployees(){
-        List<Employee> employees = employeeDAO.findAll();
+        List<Employee> employees = employeeService.findAll();
         return new ResponseEntity<>(employees, HttpStatus.OK);
     }
 }
