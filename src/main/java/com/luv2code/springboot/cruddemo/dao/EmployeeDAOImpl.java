bd248092274c23decb4f8d10ecfee48f55de11bd
0x0desha74 <mudesha74@gmail.com>
2025-05-20 22:33:01 +0300

    Employee DAO

---
+++ src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOImpl.java
@@ -0,0 +1,24 @@
+package com.luv2code.springboot.cruddemo.dao;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+    private final EntityManager entityManager;
+
+    public EmployeeDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        TypedQuery<Employee> query = entityManager.createQuery("From Employee", Employee.class);
+        return query.getResultList();
+    }
+}
